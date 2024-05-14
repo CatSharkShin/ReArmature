dde28a22da12b1cfb979b9b011a10024968abc26
CatSharkShin <catsharkshin@gmail.com>
2024-05-14 17:50:31 +0200

    button process reworked. bone stripping fixed. stuff refactored

--- ReArmature.cs
+++ ReArmature.cs
@@ -8,6 +8,7 @@ using SkinnedMeshRenderer = FrooxEngine.SkinnedMeshRenderer;
 using System.Reflection;
 using Elements.Assets;
 using Elements.Core;
+using System.Collections;
 namespace ReArmature
 {
     public class ReArmature : ResoniteMod
@@ -16,7 +17,7 @@ namespace ReArmature
 
         public override string Author => "CatShark";
 
-        public override string Version => "2.1.1";
+        public override string Version => "2.1.2";
         public override void OnEngineInit()
         {
             Harmony harmony = new Harmony("net.catshark.rearmature");
@@ -32,81 +33,104 @@ namespace ReArmature
             {
                 var btn = ui.Button("Re-Setup bones");
                 btn.LocalPressed += (button, data) => {
-                    Slot armature = findSlotByName(__instance.Slot.GetObjectRoot(), "Armature");
-                    SetupBones(__instance, armature);
+                    __instance.Slot.StartCoroutine(Process(button,data,__instance));
                 };
             }
+
+            private static IEnumerator<Context> Process(IButton button,ButtonEventData data,SkinnedMeshRenderer skmr)
+            {
+                if (skmr.Mesh.Asset?.Data == null)
+                {
+                    button.LabelText = "Cannot setup bones without a loaded asset";
+                    yield break;
+                }
+                MethodInfo StripEmptyBones = skmr.GetType().GetMethod("StripEmptyBones", BindingFlags.NonPublic | BindingFlags.Instance);
+                StripEmptyBones.Invoke(skmr, new object[] { button, data });
+                
+                int elapsedUpdates = 0;
+                while (button.Enabled == false || skmr.Mesh.Asset?.Data == null)
+                {
+                    yield return Context.WaitForNextUpdate();
+                    elapsedUpdates++;
+                    if (elapsedUpdates > 600)
+                    {
+                        button.LabelText = "Cannot setup bones without a loaded asset";
+                        yield break;
+                    }
+                }
+
+                Slot armature = skmr.Slot.GetObjectRoot().FindChild(s => s.Name == "Armature");
+                button.LabelText = SetupBones(skmr, armature);
+
+                yield return Context.WaitForSeconds(5);
+                button.LabelText = "Re-Setup bones";
+            }
         }
 
-        public static Slot findSlotByName(Slot slot, string name)
-        {
-            Slot armature = slot.FindChild(s => s.Name == name);
-            return armature;
-        }
-
-        public static void SetupBones(SkinnedMeshRenderer skmr,Slot root)
+        public static string SetupBones(SkinnedMeshRenderer skmr,Slot root)
         {
             MethodInfo GetBoneCandidates = skmr.GetType().GetMethod("GetBoneCandidates", BindingFlags.NonPublic | BindingFlags.Instance);
-
-            MeshX val = skmr.Mesh.Asset?.Data;
-            if (val == null)
+            MeshX mesh = skmr.Mesh.Asset?.Data;
+            if (root == null)
             {
-                throw new Exception("Cannot setup bones without a loaded asset");
+                return "Cannot setup bones without an Armature (not found)";
             }
             Dictionary<string, Slot> candidateDictionary = Pool.BorrowDictionary<string, Slot>();
-            val.StripEmptyBones();
+
+            // Attach missing bones
             do
             {
                 GetBoneCandidates.Invoke(skmr, new object[] { root, candidateDictionary});
-            } while (!attachMissingBones(candidateDictionary, skmr));
+            } while (attachMissingBones(candidateDictionary, skmr));
             GetBoneCandidates.Invoke(skmr, new object[] { root, candidateDictionary });
 
+            // Clear old bones and add new ones from the new armature
             skmr.Bones.Clear();
-            for (int i = 0; i < val.BoneCount; i++)
+            for (int i = 0; i < mesh.BoneCount; i++)
             {
-                string name = val.GetBone(i).Name;
+                string name = mesh.GetBone(i).Name;
                 candidateDictionary.TryGetValue(name, out var value);
                 skmr.Bones.Add().Target = value;
             }
             Pool.Return<string, Slot>(ref candidateDictionary);
+
+            return "Bones setup";
         }
 
         /// <summary>
-        /// Attaches BoneGroups it can't find in the bones. returns if there was a bonegroup to attach.
+        /// Attaches Slots it cant find in the new Armature.
         /// Attaches a duplicate.
         /// </summary>
         /// <param name="bones">Armature's bones to attach to</param>
-        /// <param name="skmr">These Renderers missing bones in the armature will be attached</param>
+        /// <param name="skmr">This Renderer's missing bones in the armature will be attached</param>
         private static bool attachMissingBones(Dictionary<string,Slot> bones,SkinnedMeshRenderer skmr)
         {
             for (int i = 0; i < skmr.Bones.Count; i++)
             {
-                string name = skmr.Bones[i].Name;
-                if (!bones.ContainsKey(name))
+                if (!bones.ContainsKey(skmr.Bones[i].Name))
                 {
                     attachBoneGroup(skmr.Bones[i], bones);
-                    return false;
+                    return true;
                 }
             }
-            return true;
+            return false;
         }
 
         /// <summary>
         /// Finds the root of the bonegroup and attaches a duplicate.
         /// </summary>
-        /// <param name="boneRootToAttach"></param>
+        /// <param name="boneToAttach"></param>
         /// <param name="newArmatureDictionary"></param>
-        private static void attachBoneGroup(Slot boneRootToAttach,Dictionary<string, Slot> newArmatureDictionary)
+        private static void attachBoneGroup(Slot boneToAttach,Dictionary<string, Slot> newArmatureDictionary)
         {
-            if(newArmatureDictionary.TryGetValue(boneRootToAttach.Parent.Name, out var value))
+            if(newArmatureDictionary.TryGetValue(boneToAttach.Parent.Name, out var value))
             {
-                boneRootToAttach.Duplicate(value,false);
+                boneToAttach.Duplicate(value,false);
             }
             else
             {
-                attachBoneGroup(boneRootToAttach.Parent, newArmatureDictionary);
+                attachBoneGroup(boneToAttach.Parent, newArmatureDictionary);
             }
-            
         }
 
     }
